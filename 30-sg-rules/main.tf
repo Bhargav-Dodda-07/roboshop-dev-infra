6ebb74b0df095e4bd03cf1c3f339cba63a55ffd1
bhargav-devops-7 <bhargavdodda996@gmail.com>
2026-02-25 10:01:12 +0530

    roboshop-sg-rules

---
+++ 30-sg-rules/main.tf
@@ -0,0 +1,23 @@
+# Backend ALB accepting traffic from Bastion
+
+resource "aws_security_group_rule" "backend_alb_bastion" {
+
+   type = "ingress"
+   security_group_id = local.backend_alb_sg_id 
+   source_security_group_id = local.bastion_sg_id
+   from_port         = 80
+   protocol       = "tcp"
+   to_port           = 80
+}
+
+# Bastion accepting traffic from All
+
+resource "aws_security_group_rule" "bastion_laptop" {
+
+   type = "ingress"
+   security_group_id = local.bastion_sg_id 
+   cidr_blocks = ["0.0.0.0/0"]
+   from_port         = 22
+   protocol       = "tcp"
+   to_port           = 22
+}
