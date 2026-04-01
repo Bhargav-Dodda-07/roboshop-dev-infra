40e08a0426741a6d5be969e13f62846e0e2b578e
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-04-01 14:15:34 +0530

    terraform-roboshop-databases

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -39,3 +39,45 @@ resource "terraform_data" "mongodb" {
     ]
   }
 }
+
+resource "aws_instance" "redis" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.redis_sg_id]
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-redis"
+    }
+  )
+}
+
+resource "terraform_data" "redis" {
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.redis.private_ip
+  }
+
+  # terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    # command = "bootstrap-hosts.sh"
+
+    inline = [
+        "chmod -x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh"
+    ]
+  }
+}
