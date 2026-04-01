55fd810616c11ac57a1893dd0ef1ac11e5789e9e
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-04-01 11:12:00 +0530

    roboshop-dev-databases

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,6 +1,6 @@
 locals {
     common_name_suffix = "${var.project_name}-${var.environment}"   # roboshop-dev
-    ami_id = data.aws_instance.joindevops.id
+    ami_id = data.aws_ami.joindevops.id
     database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 
