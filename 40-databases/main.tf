cc65c96fb1f3b68009542dbb6e029e19fbf05c68
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-04-01 15:56:33 +0530

    terraform-roboshop-databases

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -67,7 +67,7 @@ resource "terraform_data" "redis" {
     host     = aws_instance.redis.private_ip
   }
 
-  # terraform copies this file to mongodb server
+  # terraform copies this file to REDIS server
   provisioner "file" {
     source = "bootstrap.sh"
     destination = "/tmp/bootstrap.sh"
@@ -79,6 +79,90 @@ resource "terraform_data" "redis" {
     inline = [
         "chmod -x /tmp/bootstrap.sh",
         "sudo sh /tmp/bootstrap.sh redis"
+    ]
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-rabbitmq"
+    }
+  )
+}
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+  }
+
+  # terraform copies this file to RABBITMQ server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    # command = "bootstrap-hosts.sh"
+
+    inline = [
+        "chmod -x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh rabbitmq"
+    ]
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.mysql_sg_id]
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-mysql"
+    }
+  )
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mysql.private_ip
+  }
+
+  # terraform copies this file to MYSQL server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    # command = "bootstrap-hosts.sh"
+
+    inline = [
+        "chmod -x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql"
     ]
   }
 }
