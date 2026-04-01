437e43d55408ddaf3d018d31fc7041371c9d04ef
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-04-01 13:34:51 +0530

    terraform-roboshop-databases

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 dnf install ansible -y
-ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/Bhargav-Dodda-07/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
