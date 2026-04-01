a663ba212d995b0d58f17d54e3ad5a6ca6bf94ff
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-04-01 16:16:21 +0530

    terraform-roboshop-sg-rules

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -45,3 +45,27 @@ resource "aws_security_group_rule" "redis_bastion" {
    protocol       = "tcp"
    to_port           = 22
 }
+
+# RABBITMQ accepting traffic from Bastion
+
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+
+   type = "ingress"
+   security_group_id = local.rabbitmq_sg_id 
+   source_security_group_id = local.bastion_sg_id
+   from_port         = 22
+   protocol       = "tcp"
+   to_port           = 22
+}
+
+# MYSQL accepting traffic from Bastion
+
+resource "aws_security_group_rule" "mysql_bastion" {
+
+   type = "ingress"
+   security_group_id = local.mysql_sg_id 
+   source_security_group_id = local.bastion_sg_id
+   from_port         = 22
+   protocol       = "tcp"
+   to_port           = 22
+}
