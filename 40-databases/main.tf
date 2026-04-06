402563aaa043ccd7598f00c6086e458c9f1209ce
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-04-06 14:17:16 +0530

    terraform-roboshop-databases

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -142,7 +142,7 @@ resource "aws_instance" "mysql" {
 
 resource "aws_iam_instance_profile" "mysql" {
   name = "mysql"
-  role = "EC2SSMParameterRead"
+  role = "EC2ParameterRead"
 }
 
 resource "terraform_data" "mysql" {
