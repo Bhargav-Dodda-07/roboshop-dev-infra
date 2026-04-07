77b4edafde3ec9dc6ddce87a3dda6662956fff66
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-04-07 14:08:56 +0530

    terraform-roboshop-databases

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -172,3 +172,44 @@ resource "terraform_data" "mysql" {
     ]
   }
 }
+
+
+# Creating R53 record for MONGODB
+resource "aws_route53_record" "mongodb" {
+  zone_id = var.zone_id
+  name    = "mongodb-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mongodb.private_ip]
+  allow_overwrite = true
+}
+
+# Creating R53 record for REDIS
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
+}
+
+# Creating R53 record for RABBITMQ
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name    = "rabbitmq-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
+}
+
+# Creating R53 record for MYSQL
+resource "aws_route53_record" "mysql" {
+  zone_id = var.zone_id
+  name    = "mysql-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
+}
