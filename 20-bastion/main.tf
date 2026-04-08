efd7d0e137166c477f9bbab49f3861588193f475
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-04-08 19:20:17 +0530

    terraform-roboshop-bastion

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -5,6 +5,13 @@ resource "aws_instance" "bastion" {
     vpc_security_group_ids = [local.bastion_sg_id]
     iam_instance_profile = aws_iam_instance_profile.bastion.name
 
+    # Define the root volume size
+    root_block_device {
+        volume_size = 50           # Size in GiB
+        volume_type = "gp3"        # General Purpose SSD (gp3 is recommended over gp2)
+        delete_on_termination = true
+    }
+
     user_data = file("bastion.sh")
     tags = merge(
         local.common_tags,
