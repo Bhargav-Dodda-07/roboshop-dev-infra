835a4b20f39529dc2fc2850b5fbe5c4f5d7671c7
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-04-08 19:28:50 +0530

    terraform-roboshop-databases

--- 40-databases/provider.tf
+++ 40-databases/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket         = "remote.state.86s.dev"
-    key            = "roboshop-dev-databases"
+    key            = "roboshop-dev-database"
     region         = "us-east-1"
     use_lockfile   = true
     encrypt        = true
