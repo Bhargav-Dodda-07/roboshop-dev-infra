865421221256dc079e08c6ee7ffbf1846ac0cad2
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-04-09 13:56:50 +0530

    terraform-roboshop-catalogue

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -42,7 +42,7 @@ resource "terraform_data" "catalogue" {
   }
 }
 
-# stop the instance to take image
+# Stop the instance to take image
 
 resource "aws_ec2_instance_state" "catalogue" {
   instance_id = aws_instance.catalogue.id
@@ -50,6 +50,7 @@ resource "aws_ec2_instance_state" "catalogue" {
   depends_on = [terraform_data.catalogue]
 }
 
+# Create AMI 
 resource "aws_ami_from_instance" "catalogue" {
   name               = "${local.common_name_suffix}-catalogue-ami"
   source_instance_id = aws_instance.catalogue.id
@@ -62,3 +63,75 @@ resource "aws_ami_from_instance" "catalogue" {
     }
   )
 }
+
+# Create Target Group
+resource "aws_lb_target_group" "alb-example" {
+  name        = "${local.common_name_suffix}-catalogue"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = local.vpc_id
+  deregistration_delay = 60 # waiting period before deleting the instance
+
+  health_check {
+    healthy_threshold = 2
+    interval = 10
+    matcher = "200-299"
+    path = "/health"
+    port = 8080
+    protocol = "HTTP"
+    timeout = 2
+    unhealth_threshold = 2
+  }
+}
+
+
+# Create a Launch Template
+resource "aws_launch_template" "catalogue" {
+  name = "${local.common_name_suffix}-catalogue"
+  
+  image_id = aws_ami_from_instance.catalogue.id
+
+  instance_initiated_shutdown_behavior = "terminate"
+  
+  instance_type = "t3.micro"
+
+  
+
+  vpc_security_group_ids = [local.catalogue_sg_id]
+
+
+  # tags attached to the instance
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name_suffix}-catalogue"
+      }
+    )
+  }
+  
+  # tags attached to the volume created instance
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name_suffix}-catalogue"
+      }
+    )
+  }
+
+  # tags attached to the launch template
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-catalogue"
+    }
+  )
+}
+
+  
